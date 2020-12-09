d38a17d195f062168762e4c93fdbf88786b1242c
Barbara Kogus <barbarakogus@gmail.com>
2020-12-09 19:48:57 +0100

    Adiçao da tabela no arquivo contato.html, configuraçoes de estilo no arquivo style_barbara.css para o elemento recem criado(tabela) e adiçao de um arquivo js (index.js) ja referenciado na tag <script>

---
+++ index.js
@@ -0,0 +1,10 @@
+const enviarFormulario = document.querySelector(".enviar");
+
+const conteudoTabela = document.querySelector(".conteudo-tabela");
+
+enviarFormulario.addEventListener("click", function(){
+        
+     
+    
+});
+
