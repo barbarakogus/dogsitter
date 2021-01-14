0168a5558eb4eada16a07e517fdb583fd0eee3eb
Barbara Kogus <barbarakogus@gmail.com>
2021-01-14 18:58:16 +0100

    separaçao do codigo JS em funçoes, validaçoes das entradas dos input.

--- index.js
+++ index.js
@@ -6,54 +6,84 @@ enviarFormulario.addEventListener("click", function(event){
     event.preventDefault();
 
     const form = document.querySelector("form");
+
+    const cliente = obterDadosDoFormulario (form);
+
+    validarFormulario (cliente);
     
-    let nome = form.nome.value;
-    let nomePet = form.nomePet.value;
-    let email = form.email.value;
-    let telefone = form.telefone.value;
-    let mensagem = form.mensagem.value;
-    let contato = form.contato.value;
-    let periodo = form.periodo.value;
-    let checkbox = form.cheackbox.checked;
-    
-    let clienteTr = document.createElement("tr");
-    let nomeTd = document.createElement("td");
-    nomeTd.classList.add("conteudo-tabela");
-    let nomePetTd = document.createElement("td");
-    nomePetTd.classList.add("conteudo-tabela");
-    let emailTd = document.createElement("td");
-    emailTd.classList.add("conteudo-tabela");
-    let telefoneTd = document.createElement("td");
-    telefoneTd.classList.add("conteudo-tabela");
-    let mensagemTd = document.createElement("td");
-    mensagemTd.classList.add("conteudo-tabela");
-    let contatoTd = document.createElement("td");
-    contatoTd.classList.add("conteudo-tabela");
-    let periodoTd = document.createElement("td");
-    periodoTd.classList.add("conteudo-tabela");
-    let checkboxTd = document.createElement("td");
-    checkboxTd.classList.add("conteudo-tabela");
+    if(!validarFormulario(cliente)){
+        return;
+    };
 
-    nomeTd.textContent = nome;
-    nomePetTd.textContent = nomePet;
-    emailTd.textContent = email;
-    telefoneTd.textContent = telefone;
-    mensagemTd.textContent = mensagem;
-    contatoTd.textContent = contato;
-    periodoTd.textContent = periodo;
-    checkboxTd.textContent = checkbox;
-
-    clienteTr.appendChild(nomeTd);
-    clienteTr.appendChild(nomePetTd);
-    clienteTr.appendChild(emailTd);
-    clienteTr.appendChild(telefoneTd);
-    clienteTr.appendChild(mensagemTd);
-    clienteTr.appendChild(contatoTd);
-    clienteTr.appendChild(periodoTd);
-    clienteTr.appendChild(checkboxTd);
-
+    const clienteTr = criarTr(cliente);
+ 
     tabela.appendChild(clienteTr);
-     
-    
+
+    form.reset();
 });
+
+function obterDadosDoFormulario (form){
+    let cliente = {
+        nome: form.nome.value,
+        nomePet: form.nomePet.value,
+        email: form.email.value,
+        telefone: form.telefone.value,
+        mensagem: form.mensagem.value,
+        contato: form.contato.value,
+        periodo: form.periodo.value,
+        checkbox: form.cheackbox.checked ? "sim" : "não" //if ternário(?): if checkbox.checked for true, assume "sim", se n "não". Usado para validações pequenas, simples.
+    }
+  //  if(cliente.checkbox){
+  //      cliente.checkbox = "sim";
+  //  }else {
+  //      cliente.checkbox = "não";
+  //  }
+    return cliente;
+}
+
+function criarTr (cliente){
+    let tr = document.createElement("tr");
+
+    tr.appendChild(criarTd(cliente.nome, "conteudo-tabela"));
+    tr.appendChild(criarTd(cliente.nomePet, "conteudo-tabela"));
+    tr.appendChild(criarTd(cliente.email, "conteudo-tabela"));
+    tr.appendChild(criarTd(cliente.telefone, "conteudo-tabela"));
+    tr.appendChild(criarTd(cliente.mensagem, "conteudo-tabela"));
+    tr.appendChild(criarTd(cliente.contato, "conteudo-tabela"));
+    tr.appendChild(criarTd(cliente.periodo, "conteudo-tabela"));
+    tr.appendChild(criarTd(cliente.checkbox, "conteudo-tabela"));
+
+    return tr;
+}
+
+function criarTd (dado,classe){
+    let td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+    return td;
+}
+
+function validarFormulario (cliente, span){
+    let estaValido = true;
+    const span = document.querySelectorAll(".span");   
+    
+    if(cliente.nome.length == ""){
+        span[0].textContent = "O campo nome é indispensável";
+        estaValido = false;
+        return;
+    }
+    if(cliente.email.length == ""){
+        span[1].textContent = "O campo email é indispensável";
+        estaValido = false;
+        return;
+    }
+    if(cliente.telefone.length == ""){
+        span[2].textContent = "O campo telefone é indispensável";
+        estaValido = false;
+        return;
+    };
+    return estaValido;
+}
+
+
 
